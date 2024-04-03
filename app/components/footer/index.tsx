6cbae2d9b4681117529124fc06037af09074f719
Emre Ozturk <emreeozturk42@gmail.com>
2024-04-03 21:17:23 +0300

    Add footer component

---
+++ app/components/footer/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div
+            className='h-24  bg-orange-500 flex justify-center items-center text-white text-2xl w-full'
+        >
+            <p>
+                &copy; 2024 All rights reserved Developed by Emre.
+            </p>
+        </div>
+    )
+}
+
+export default Footer
