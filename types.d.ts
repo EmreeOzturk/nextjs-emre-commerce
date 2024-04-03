8af8b08b215a563c49eb0ffceced7b4a99be824d
Emre Ozturk <emreeozturk42@gmail.com>
2024-04-03 21:16:02 +0300

    Add types for Product, Review, and User

---
+++ types.d.ts
@@ -0,0 +1,33 @@
+export type Product = {
+  id: string;
+  name: string;
+  description: string;
+  price: number;
+  brand: string;
+  category: string;
+  inStock: boolean;
+  image: string;
+  reviews: Review[];
+};
+
+export type Review = {
+  id: string;
+  userId: string;
+  productId: string;
+  rating: number;
+  comment: string;
+  createdDate: string;
+  user: User;
+};
+
+export type User = {
+  id: string;
+  name: string;
+  email: string;
+  emailVerified: boolean | null;
+  image: string;
+  hashedPassword: string | null;
+  createdAt: string;
+  updatedAt: string;
+  role: string;
+};
