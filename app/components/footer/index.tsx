294801f0b8c2bf81e5674fe3d1544b77b37d1c0c
Emre Ozturk <emreeozturk42@gmail.com>
2024-04-04 02:04:59 +0300

    Update footer copyright message

--- app/components/footer/index.tsx
+++ app/components/footer/index.tsx
@@ -6,7 +6,7 @@ const Footer = () => {
             className='h-24  bg-orange-500 flex justify-center items-center text-white text-2xl w-full'
         >
             <p>
-                &copy; 2024 All rights reserved Developed by Emre.
+                &copy; 2024 Developed by Emre.
             </p>
         </div>
     )
