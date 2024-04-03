74485529d017f8ba2170fb5bb6f93b28a8da19a4
Emre Ozturk <emreeozturk42@gmail.com>
2024-04-03 21:17:04 +0300

    Add Logo component

---
+++ app/components/common/Logo.tsx
@@ -0,0 +1,8 @@
+"use client";
+const Logo = () => {
+  return (
+    <div className="bg-orange-700 px-2 py-1 rounded-lg text-lg md:text-2xl cursor-pointer">Emre.<span className="text-sm">Commerce</span></div>
+  )
+}
+
+export default Logo
