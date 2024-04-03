c2ffb53c7954f15289ed45cb93ee67a1d8d500c9
Emre Ozturk <emreeozturk42@gmail.com>
2024-04-03 21:18:18 +0300

    Add ProductReview component

---
+++ app/components/reviews/ProductReview.tsx
@@ -0,0 +1,40 @@
+import { Review } from "@/types"
+import Image from "next/image"
+import RatingStars from "../common/RatingStars"
+
+
+type ProductReviewProps = {
+    review: Review
+}
+const ProductReview: React.FC<ProductReviewProps> = (
+    { review }
+) => {
+    return (
+        <div className="flex w-full md:w-1/3 shadow-lg pt-4 border rounded-lg gap-4 md:gap-10">
+            <div className="flex flex-col gap-2 justify-center items-center">
+                <div className="w-10 h-10 relative border border-black rounded-full">
+                    <Image src="/ip.avif" alt="name" fill className="rounded-full" />
+                </div>
+                <div>
+                    {review.user.name}
+                </div>
+                <div className="scale-[.8]">
+                    <RatingStars value={review.rating} />
+                </div>
+            </div>
+            <div className="flex flex-col items-start justify-start">
+                <div>
+                    {
+                        new Date(review.createdDate).toLocaleDateString()
+                    }
+                </div>
+                <div>
+                    {review.comment}
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default ProductReview
