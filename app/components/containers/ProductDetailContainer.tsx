8f80d8cb0007e27401815108a12fc819d437d2e0
Emre Ozturk <emreeozturk42@gmail.com>
2024-04-05 06:45:43 +0300

    Refactor ProductDetailContainer to support column layout

--- app/components/containers/ProductDetailContainer.tsx
+++ app/components/containers/ProductDetailContainer.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
 const ProductDetailContainer = (
-    { children }: { children: React.ReactNode }
+    { children, col }: { children: React.ReactNode, col?: boolean }
 ) => {
     return (
         <div
-            className='flex flex-col md:flex-row gap-5 md:gap-10 md:px-10 px-3 py-5 md:py-10 bg-slate-100 w-full h-full'
+            className={`flex flex-col md:flex-row gap-5 md:gap-10 md:px-10 px-3 py-5 md:py-10 bg-slate-100 w-full h-full ${col && 'md:flex-col'} `}
         >
             {children}
         </div>
