76e5618e2962f063a34feb70ca3784884782f897
Emre Ozturk <emreeozturk42@gmail.com>
2024-04-05 06:45:31 +0300

    Refactor Logo component to use Link from next/link

--- app/components/common/Logo.tsx
+++ app/components/common/Logo.tsx
@@ -1,7 +1,7 @@
-"use client";
+import Link from 'next/link'
 const Logo = () => {
   return (
-    <div className="bg-orange-700 px-2 py-1 rounded-lg text-lg md:text-2xl cursor-pointer">Emre.<span className="text-sm">Commerce</span></div>
+    <Link href={"/"} className="bg-orange-700 px-2 py-1 rounded-lg text-lg md:text-2xl cursor-pointer">Emre.<span className="text-sm">Commerce</span></Link>
   )
 }
 
