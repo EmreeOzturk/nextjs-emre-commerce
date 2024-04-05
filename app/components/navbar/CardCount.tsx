4e3538c10df73d11194dd05a416295c883ad8fc9
Emre Ozturk <emreeozturk42@gmail.com>
2024-04-05 06:45:49 +0300

    Refactor CardCount component to display cart count

--- app/components/navbar/CardCount.tsx
+++ app/components/navbar/CardCount.tsx
@@ -1,10 +1,21 @@
-import React from 'react'
+"use client"
 
+import { MdShoppingBasket } from 'react-icons/md'
+import { useCart } from '@/store/useCart'
+import Link from 'next/link'
 const CardCount = () => {
+    const items = useCart((state) => state.items)
     return (
-        <div
-            className='hidden md:flex'
-        >CardCount</div>
+        <Link href='/cart'
+            className='hidden md:flex relative justify-center items-center  rounded-full w-10 h-10'
+        >
+            <MdShoppingBasket size={25} />
+            <div>
+                <span className='absolute top-0 -right-2 bg-red-500 text-white rounded-full w-5 h-5 flex justify-center items-center text-xs'>
+                    {items.length}
+                </span>
+            </div>
+        </Link>
     )
 }
 
